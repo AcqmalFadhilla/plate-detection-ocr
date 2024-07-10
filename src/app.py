6b7d77060a141ed907e18b4b7bd69dd28a30aeec
Muhammad Acqmal Fadhilla Latief <109632348+AcqmalFadhilla@users.noreply.github.com>
2024-07-10 16:51:16 +0800

    update app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 def main():
-    st.set_page_config(initial_sidebar_state="collapsed")
     image_page = st.Page("pages/image.py", title="image")
     video_page = st.Page("pages/video.py", title="video")
 
