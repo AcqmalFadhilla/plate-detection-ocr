bf8fd453ae9f05d0c684547f23116daa3a2a0b49
Muhammad Acqmal Fadhilla Latief <109632348+AcqmalFadhilla@users.noreply.github.com>
2024-07-10 17:32:30 +0800

    fix issue

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    image_page = st.Page("./src/pages/image.py", title="image")
-    video_page = st.Page("./src/pages/video.py", title="video")
+    image_page = st.Page("pages/image.py", title="image")
+    video_page = st.Page("pages/video.py", title="video")
 
     pg = st.navigation([image_page, video_page])
     pg.run()
--- src/pages/video.py
+++ src/pages/video.py
@@ -7,7 +7,6 @@ model = YOLO("results/models/best.pt")
 
 data = st.file_uploader("chose file", type=["mp4", "mov", "avi", "mkv"])
 skip_frames = st.sidebar.slider("Skip frames", min_value=1, max_value=60, value=1, step=1, disabled=False)
-print(skip_frames)
 
 if data is not None:
     st.video(data)
